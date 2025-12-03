ba18882f5c1dd3cd848d324e811f9c81540f2605
Apollo <apollomacabodbod8@gmail.com>
2025-12-03 15:41:47 +0800

    remove unused variable

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,5 +1,4 @@
 import HomeHeroSection from "./_components/home-hero-section";
-import HomeHeroSectionCards from "./_components/home-hero-section-cards";
 
 export default function Home() {
   return (
