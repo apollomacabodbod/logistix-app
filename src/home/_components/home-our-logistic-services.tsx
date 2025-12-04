0bd941917d750caf671ce6e230db535a1ccd489d
Apollo <apollomacabodbod8@gmail.com>
2025-12-04 15:22:34 +0800

    added smooth animation

--- src/home/_components/home-our-logistic-services.tsx
+++ src/home/_components/home-our-logistic-services.tsx
@@ -1,31 +1,84 @@
 import border from "src/assets/our-logistic-services/border.svg";
+import { motion } from "framer-motion";
 
 export default function HomeOurLogisticServices() {
   return (
     <>
       <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] mt-[6.25em] gap-[2em]">
         <div className="flex flex-col">
-          <p className="text-[#FF7900] font-rajdhani text-[1.125rem] font-semibold uppercase text-center">
+          <motion.p
+            className="text-[#FF7900] font-rajdhani text-[1.125rem] font-semibold uppercase text-center"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
             Services
-          </p>
+          </motion.p>
 
-          <p className="text-[#202E30] font-rajdhani text-[3.1875rem] font-semibold  text-center">
+          <motion.p
+            className="text-[#202E30] font-rajdhani text-[3.1875rem] font-semibold  text-center"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
             Our Logistic Services
-          </p>
+          </motion.p>
         </div>
 
-        <div className="flex items-center justify-center">
+        <motion.div
+          className="flex items-center justify-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           <img
             src={border}
             alt=""
             className="object-cover object-center w-[6.25em]"
           />
-        </div>
+        </motion.div>
 
-        <p className="text-[#202E30] font-mulish text-[1rem] font-normal text-center  self-center lg:w-[40.625em]">
+        <motion.p
+          className="text-[#202E30] font-mulish text-[1rem] font-normal text-center  self-center lg:w-[40.625em]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           Vel sagittis quam sodales sederat porta nunc amet malesuada consequat
           blandit sitac amet orci gravida vulputate
-        </p>
+        </motion.p>
       </div>
     </>
   );
