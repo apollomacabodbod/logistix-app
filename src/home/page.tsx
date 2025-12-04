21a283e36ca65ddb9b3a335cbd36468e7d50d9f7
Apollo <apollomacabodbod8@gmail.com>
2025-12-04 16:15:36 +0800

    added card component

--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,6 +1,7 @@
 import HomeHeroSection from "./_components/home-hero-section";
 import HomeLogisticSolution from "./_components/home-logistic-solution";
 import HomeOurLogisticServices from "./_components/home-our-logistic-services";
+import HomeOurLogisticServicesCards from "./_components/home-our-logistic-services-cards";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HomeHeroSection />
       <HomeLogisticSolution />
       <HomeOurLogisticServices />
+      <HomeOurLogisticServicesCards />
     </>
   );
 }
