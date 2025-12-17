6ac0b95e31fdf8d90e6c97bea0ba918f8f4aebe2
Apollo <apollomacabodbod8@gmail.com>
2025-12-17 14:14:50 +0800

    added smooth animation

--- src/home/_components/home-testimontials-cards.tsx
+++ src/home/_components/home-testimontials-cards.tsx
@@ -1,15 +1,40 @@
 import portraitOfManager from "src/assets/testimontials/portrait-of-manager.jpg";
 import doubleQuotes from "src/assets/testimontials/double-quotes.svg";
+import { motion } from "framer-motion";
 export default function HomeTestimontialsCards() {
   return (
     <>
       <div className="grid lg:grid-cols-2 max-w-[1200px] lg:mx-auto  px-[1.25em] gap-[6.181875em] mt-[4em]">
-        <div
+        <motion.div
           className="flex flex-col relative aspect-[521.09/400]  bg-cover  bg-top lg:bg-center"
           style={{ backgroundImage: `url(${portraitOfManager})` }}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
         >
           <div className="hidden lg:block absolute left-[86.35%] top-[34.25%]">
-            <div className="flex flex-col bg-[#D42B22] w-[8em] h-[8em] rounded-[8em]">
+            <motion.div
+              className="flex flex-col bg-[#D42B22] w-[8em] h-[8em] rounded-[8em]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               <div className="flex items-center justify-center my-auto">
                 <img
                   src={doubleQuotes}
@@ -17,28 +42,67 @@ export default function HomeTestimontialsCards() {
                   className="object-cover object-center w-[3em]"
                 />
               </div>
-            </div>
+            </motion.div>
           </div>
-        </div>
+        </motion.div>
 
         {/*  */}
 
         <div className="flex flex-col gap-[4em] ">
-          <p className="text-[#202E30] font-openSans text-[1.5rem] font-normal">
+          <motion.p
+            className="text-[#202E30] font-openSans text-[1.5rem] font-normal"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
             Vel sagittis quam sodales sed. Erat porta nunc, amet malesuada
             consequat blandit sit. Ac amet, orci, gravida vulputate hendrerit
             vitae feugiat ultrices. Ornare netus accumsan, felis vitae, mauris
             iaculis semper viverra. Vel sagittis quam sodales sed.
-          </p>
+          </motion.p>
 
           <div className="flex flex-col ">
-            <p className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold">
+            <motion.p
+              className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               John Adam
-            </p>
+            </motion.p>
 
-            <p className="text-[#798283] font-mulish text-[0.875rem] font-bold uppercase">
+            <motion.p
+              className="text-[#798283] font-mulish text-[0.875rem] font-bold uppercase"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               Founder - Mega international
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
