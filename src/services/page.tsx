7b299b8979c8543aae1742add6cab41629cd3f08
Apollo <apollomacabodbod8@gmail.com>
2025-12-18 14:21:45 +0800

    added logistic cards

--- src/services/page.tsx
+++ src/services/page.tsx
@@ -1,11 +1,13 @@
 import ServicesHeroSection from "./_components/services-hero-section";
 import ServicesOurLogistic from "./_components/services-our-logistic";
+import ServicesOurLogisticCards from "./_components/services-our-logistic-cards";
 
 export default function Services() {
   return (
     <>
       <ServicesHeroSection />
       <ServicesOurLogistic />
+      <ServicesOurLogisticCards />
     </>
   );
 }
