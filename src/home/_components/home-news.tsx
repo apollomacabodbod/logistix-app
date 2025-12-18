3959359342f9d452a37316ca4000b3a00739de46
Apollo <apollomacabodbod8@gmail.com>
2025-12-18 09:39:55 +0800

    added smooth animation

--- src/home/_components/home-news.tsx
+++ src/home/_components/home-news.tsx
@@ -3,97 +3,283 @@ import border from "src/assets/news/border.svg";
 import courierDriving from "src/assets/news/courier-driving.jpg";
 import courierWithParcel from "src/assets/news/courier-with-parcel.jpg";
 import warehouseOrStorage from "src/assets/news/warehouse-or-storage.jpg";
+import { motion } from "framer-motion";
 
 export default function HomeNews() {
   return (
     <>
       <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] my-[6.25em]">
-        <p className="text-[#FF7900] font-rajdhani text-[1.125rem] font-semibold uppercase">
+        <motion.p
+          className="text-[#FF7900] font-rajdhani text-[1.125rem] font-semibold uppercase"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
           NEWS
-        </p>
+        </motion.p>
 
         <div className="sm:flex sm:items-center sm:justify-between">
-          <p className="text-[#202E30] font-rajdhani text-[3.1875rem] font-semibold ">
+          <motion.p
+            className="text-[#202E30] font-rajdhani text-[3.1875rem] font-semibold "
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
             Logistix News
-          </p>
+          </motion.p>
 
           <button className="flex items-center self-start border border-[#D42B22] py-[1em] px-[2em] gap-[1em] mt-[0.5em] sm:mt-[0em]">
-            <p className="text-[#D42B22] font-rajdhani text-[1rem] font-bold uppercase">
+            <motion.p
+              className="text-[#D42B22] font-rajdhani text-[1rem] font-bold uppercase"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               See all news
-            </p>
-            <img
+            </motion.p>
+            <motion.img
               src={redArrow}
               alt=""
               className="object-cover object-center w-[0.75em]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
             />
           </button>
         </div>
 
-        <img
+        <motion.img
           src={border}
           alt=""
           className="object-cover object-center w-[6.25em] mt-[1.5em]"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
         />
 
         <div className="md:flex md:items-start md:justify-between md:gap-[2.5em] mt-[4em]">
           <div className="flex flex-col gap-[2em]">
-            <img
+            <motion.img
               src={courierDriving}
               alt=""
               className="object-cover object-center aspect-[681/500]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
             />
 
             <div className="flex flex-col mt-[2em] gap-[1em]">
-              <p className="text-[#202E30] font-rajdhani text-[2.25rem] font-semibold ">
+              <motion.p
+                className="text-[#202E30] font-rajdhani text-[2.25rem] font-semibold "
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 Dis pellentesque fermen arcuat molestie amet nibh at quis.
-              </p>
+              </motion.p>
 
-              <p className="text-[#202E30] font-openSans text-[0.875rem] italic font-normal ">
+              <motion.p
+                className="text-[#202E30] font-openSans text-[0.875rem] italic font-normal "
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 John Doe - January 15, 2021 - 0 Comments - Healthy
-              </p>
+              </motion.p>
             </div>
 
-            <p className="text-[#202E30] font-mulish text-[1rem] font-normal ">
+            <motion.p
+              className="text-[#202E30] font-mulish text-[1rem] font-normal "
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
               Lorem ipsum dolor sit amet consec tetuer adi piscing elit aenean
               com modo ligula eget dolor aenean massa cum sociis nato que pena
               tibus et magnis dis parturient montes. Donec enim diam vulputate
               ut pharetra sit. Ultricies mi eget mauris pharetra et ultrices
               neque ornare.{" "}
-            </p>
+            </motion.p>
           </div>
 
           <div className="flex flex-col  gap-[2em]">
             <div className="flex flex-col gap-[1em]">
-              <img
+              <motion.img
                 src={courierWithParcel}
                 alt=""
                 className="object-cover object-center aspect-[475/250]"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
               />
 
-              <p className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold">
+              <motion.p
+                className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 Convallis eu aenean placerat quam platea venenatis dui interdumv
-              </p>
+              </motion.p>
 
-              <p className="text-[#202E30] font-openSans text-[0.875rem] font-normal ">
+              <motion.p
+                className="text-[#202E30] font-openSans text-[0.875rem] font-normal "
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 John Doe - January 15, 2021 - 0 Comments - Healthy
-              </p>
+              </motion.p>
             </div>
 
             <div className="flex flex-col gap-[1em]">
-              <img
+              <motion.img
                 src={warehouseOrStorage}
                 alt=""
                 className="object-cover object-center aspect-[475/250]"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
               />
 
-              <p className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold">
+              <motion.p
+                className="text-[#202E30] font-rajdhani text-[1.5625rem] font-semibold"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 Risus molestie proin quis tempor cras tristique sapien sagittis
                 eturpis
-              </p>
+              </motion.p>
 
-              <p className="text-[#202E30] font-openSans text-[0.875rem] font-normal ">
+              <motion.p
+                className="text-[#202E30] font-openSans text-[0.875rem] font-normal "
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                  },
+                }}
+              >
                 John Doe - January 15, 2021 - 0 Comments - Healthy
-              </p>
+              </motion.p>
             </div>
           </div>
         </div>
