35fb7ab5f868e9117cdd79b2213f5765f82a826a
Apollo <apollomacabodbod8@gmail.com>
2025-12-18 14:51:38 +0800

    added services testimontials

---
+++ src/services/_components/services-testimontials.tsx
@@ -0,0 +1,68 @@
+import border from "src/assets/testimontials/border.svg";
+import { motion } from "framer-motion";
+
+export default function ServicesTestimontials() {
+  return (
+    <>
+      {" "}
+      <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] gap-[2em]">
+        <div className="flex flex-col">
+          <motion.p
+            className="text-[#FF7900] text-center font-rajdhani text-[1.125rem] font-semibold uppercase"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
+            TESTIMONIALS
+          </motion.p>
+
+          <motion.p
+            className="text-[#202E30] font-rajdhani text-[3.1875rem] font-semibold text-center"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+              },
+            }}
+          >
+            What Our Costumers Are Saying
+          </motion.p>
+        </div>
+
+        <motion.div
+          className="flex items-center justify-center"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+            },
+          }}
+        >
+          <img
+            src={border}
+            alt=""
+            className="object-cover object-center w-[6.25em] "
+          />
+        </motion.div>
+      </div>
+    </>
+  );
+}
--- src/services/page.tsx
+++ src/services/page.tsx
@@ -3,6 +3,8 @@ import ServicesHeroSection from "./_components/services-hero-section";
 import ServicesLogoPartner from "./_components/services-logo-partner";
 import ServicesOurLogistic from "./_components/services-our-logistic";
 import ServicesOurLogisticCards from "./_components/services-our-logistic-cards";
+import ServicesTestimontials from "./_components/services-testimontials";
+import ServicesTestimontialsCards from "./_components/services-testimontials-cards";
 import ServicesTestimontialsYellowBox from "./_components/services-testimontials-yellow-box";
 
 export default function Services() {
@@ -14,6 +16,8 @@ export default function Services() {
       <ServicesCovid />
       <ServicesLogoPartner />
       <ServicesTestimontialsYellowBox />
+      <ServicesTestimontials />
+      <ServicesTestimontialsCards />
     </>
   );
 }
