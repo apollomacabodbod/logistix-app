f1d4987198fed74e6bfcafbed983cdf53829272d
Apollo <apollomacabodbod8@gmail.com>
2025-12-19 13:49:41 +0800

    added testimontials component

--- src/testimontials/_components/testimontials-hero-section.tsx
+++ src/testimontials/_components/testimontials-hero-section.tsx
@@ -1,7 +1,90 @@
+import deliveryService from "src/assets/testimontials/delivery-service.jpg";
+import border from "src/assets/services/border.svg";
+import whiteGreaterThan from "src/assets/services/white-greater-than.svg";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 export default function TestimontialsHeroSection() {
   return (
     <>
-      <div className=""></div>
+      <div
+        className="flex flex-col relative bg-cover bg-top "
+        style={{ backgroundImage: `url(${deliveryService})` }}
+      >
+        <div className="absolute inset-0 bg-[rgba(34,34,34,0.50)]"></div>
+        <div className="z-10">
+          <div className="flex flex-col max-w-[1200px] lg:mx-auto  px-[1.25em] mt-[11.9375em] mb-[7.3125em] gap-[2em]">
+            <motion.p
+              className="text-[#FFF] font-rajdhani text-[4.5rem] font-semibold "
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
+              Testimonials
+            </motion.p>
+
+            <motion.img
+              src={border}
+              alt=""
+              className="object-cover object-center w-[6.25em] "
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            />
+
+            <motion.div
+              className="flex items-center gap-[1em]"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: {
+                  opacity: 1,
+                  y: 0,
+                  transition: { duration: 1, ease: "easeInOut" }, // Smooth transition
+                },
+              }}
+            >
+              <Link
+                to={"/"}
+                className="text-[#FFF] font-openSans text-[1rem] font-semibold"
+              >
+                Home
+              </Link>
+
+              <img
+                src={whiteGreaterThan}
+                alt=""
+                className="object-cover object-center w-[0.625em]"
+              />
+
+              <Link
+                to={"/testimontials"}
+                className="text-[#FFF] font-openSans text-[1rem] font-semibold"
+              >
+                Testimonials
+              </Link>
+            </motion.div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
