a981eba608e6d58bdb297dd871d758340c4c6b17
Apollo <apollomacabodbod8@gmail.com>
2025-12-19 13:58:13 +0800

    added yellow box

--- src/testimontials/page.tsx
+++ src/testimontials/page.tsx
@@ -1,9 +1,11 @@
 import TestimontialsHeroSection from "./_components/testimontials-hero-section";
+import TestimontialsYellowBox from "./_components/testimontials-yellow-box";
 
 export default function Testimontials() {
   return (
     <>
       <TestimontialsHeroSection />
+      <TestimontialsYellowBox />
     </>
   );
 }
