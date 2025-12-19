c6c147c62e2d5e5d571cdf22972e444ada63920f
Apollo <apollomacabodbod8@gmail.com>
2025-12-19 14:07:53 +0800

    added testimontials cards

--- src/testimontials/page.tsx
+++ src/testimontials/page.tsx
@@ -1,4 +1,6 @@
+import TestimontialsCards from "./_components/testimontials-cards";
 import TestimontialsHeroSection from "./_components/testimontials-hero-section";
+import TestimontialsOurCustomers from "./_components/testimontials-our-customers";
 import TestimontialsYellowBox from "./_components/testimontials-yellow-box";
 
 export default function Testimontials() {
@@ -6,6 +8,8 @@ export default function Testimontials() {
     <>
       <TestimontialsHeroSection />
       <TestimontialsYellowBox />
+      <TestimontialsOurCustomers />
+      <TestimontialsCards />
     </>
   );
 }
