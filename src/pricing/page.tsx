70df9449bd6e3bd464d508ff278c39aed2d67c16
Apollo <apollomacabodbod8@gmail.com>
2025-12-23 10:12:59 +0800

    Added yellow box

--- src/pricing/page.tsx
+++ src/pricing/page.tsx
@@ -1,9 +1,11 @@
 import PricingHeroSection from "./_components/pricing-hero-section";
+import PricingYellowBox from "./_components/pricing-yellow-box";
 
 export default function Pricing() {
   return (
     <>
       <PricingHeroSection />
+      <PricingYellowBox />
     </>
   );
 }
